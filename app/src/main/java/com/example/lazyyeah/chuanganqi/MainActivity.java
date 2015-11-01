a4c7cfc1b08b5929b1fd1fdcac476bff1bc8a466
lazyyeahh <liziye123@gmail.com>
2015-11-01 13:27:24 +0800

    读取安卓手机传感器

---
+++ app/src/main/java/com/example/lazyyeah/chuanganqi/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.lazyyeah.chuanganqi;
+
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private SensorManager SM;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        SM = (SensorManager) getSystemService(SENSOR_SERVICE);
+        List<Sensor> sensors =SM.getSensorList(Sensor.TYPE_ALL);
+        for (Sensor sensor:sensors)
+        {
+            textView.
+
+
+
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
